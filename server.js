711e34134dd61b0846caa584c61d7a05b9e79cf1
James Hagerman <james.hagerman@gmail.com>
2016-02-07 02:20:50 -0800

    Cleaning up main server.js file and starting to reorganize the design pattern to be more modern.

--- server.js
+++ server.js
@@ -15,35 +15,26 @@ var Plugins = require('./src/Plugins');
 
 
 function Server() {
+  // "Constructor" for the Object. Remember, no such thing as classes in JS...
+  var _sessions = new sets.Set();
+  var _rooms = {};
+  var _userList = Array();
+  var _plugins = new Plugins(this);
 
-    this._sessions = new sets.Set();
-    this._rooms = {};
-    this._userList = Array();
-    this._plugins = new Plugins(this);
-}
+  // Properties that were on the old Server "class" object:
+  var serverObject = null;
+  var sslServerObject = null;
 
-Server.prototype.getRoom = function(roomId) {
-    if(this._rooms[roomId] === undefined)  {
-        this._rooms[roomId] = new Room(roomId);
-    }
+  return {
+    start: start,
+    startWebServer: startWebServer,
+    onConnect: onConnect,
+    getRoom: getRoom,
+    isNameFree: isNameFree,
+  };
 
-    return this._rooms[roomId];
-};
-
-// ## Check if username is in use ##
-Server.prototype.isNameFree = function(name) {
-
-    var free = true;
-    this._sessions.each(function(s) {
-        if(s.id === name) {
-            free = false;
-        }
-    });
-    return free;
-};
-
-// ## Start Socket Server ##
-Server.prototype.start = function() {
+  function start() {
+    // Start Socket Server
     console.log('========================');
     console.log('Janus VR Presence Server');
     console.log('========================');
@@ -53,86 +44,94 @@ Server.prototype.start = function() {
     console.log('Log level: ' + config.logLevel);
     console.log('Startup date/time: ' + Date());
 
-    this.server = net.createServer(this.onConnect.bind(this));
-    this.server.listen(config.port, "::", function(err){
-
-        if(err) {
-            log.error('Socket Server error listening on port: ' + config.port);
-            process.exit(1);
-        }
-
-        log.info('Socket Server listening on port: ' + config.port);
-        console.log('Socket Server listening on port: ' + config.port);
-
+    serverObject = net.createServer(onConnect);
+    server.listen(config.port, "::", function(err) {
+      if (err) {
+        log.error('Socket Server error listening on port: ' + config.port);
+        process.exit(1);
+      }
+      log.info('Socket Server listening on port: ' + config.port);
+      console.log('Socket Server listening on port: ' + config.port);
     });
 
-    if(config.ssl) {
-
-        this.ssl = tls.createServer(config.ssl.options, this.onConnect.bind(this));
-        this.ssl.listen(config.ssl.port, "::", function(err){
-
-            if(err) {
-                log.error('SSL Server error listening on port: ' + config.ssl.port);
-                process.exit(1);
-            }
-
-            console.log('SSL Server listening on port: ' + config.ssl.port);
-            log.info('SSL Server listening on port: ' + config.ssl.port);
-
-        });
+    // Only listen for SSL connections if it's configured:
+    if (config.ssl) {
+      sslServerObject = tls.createServer(config.ssl.options, onConnect);
+      sslServerObject.listen(config.ssl.port, "::", function(err) {
+        if (err) {
+          log.error('SSL Server error listening on port: ' + config.ssl.port);
+          process.exit(1);
+        }
+        console.log('SSL Server listening on port: ' + config.ssl.port);
+        log.info('SSL Server listening on port: ' + config.ssl.port);
+      });
     }
 
     this.startWebServer();
-};
-
-
-// ## start web server ##
-Server.prototype.startWebServer = function() {
-
+  }
+  function startWebServer() {
+    // start web server
     var self = this;
-
     this.ws = express();
-
-
     var router = express.Router();
 
-    router.get('/log', function(req,res){
-        res.writeHead(200, {'Content-Type':'text/plain', 'Content-Length':-1, 'Transfer-Encoding': 'chunked'});
-        var logFile = fs.createReadStream('server.log');
-        logFile.pipe(res);
+    router.get('/log', function(req, res) {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain',
+        'Content-Length': -1,
+        'Transfer-Encoding': 'chunked'
+      });
+      var logFile = fs.createReadStream('server.log');
+      logFile.pipe(res);
     });
 
-    router.get('/', function(req,res){
-        res.send(200, 'Nothing to see here ... yet');
+    router.get('/', function(req, res) {
+      res.send(200, 'Nothing to see here ... yet');
     });
-
 
     this.ws.use(router);
 
     this.ws.listen(config.webServer, "::");
     log.info('Webserver started on port: ' + config.webServer);
     console.log('Start Date/Time: ' + Date());
-};
-
-// ## action on client connection ##
-Server.prototype.onConnect = function(socket) {
-
+  }
+  function onConnect(socket) {
     var self = this;
     var addr = socket.remoteAddress;
     log.info('Client connected ' + addr);
 
-    var s = new Session(this, socket);
-    this._sessions.add(s);
+    var thisSession = new Session(this, socket);
+    _sessions.add(thisSession);
 
     socket.on('close', function() {
-        log.info('Client disconnected: ' + addr);
-        self._sessions.remove(s);
+      log.info('Client disconnected: ' + addr);
+      _sessions.remove(thisSession);
     });
 
-    socket.on('error', function(err){
-	log.error(addr);
-        log.error('Socket error: ', err);
+    socket.on('error', function(err) {
+      log.error(addr);
+      log.error('Socket error: ', err);
     });
-};
+  }
+  function getRoom(roomId) {
+    if (this._rooms[roomId] === undefined) {
+      this._rooms[roomId] = new Room(roomId);
+    }
 
-(new Server()).start();
+    return this._rooms[roomId];
+  }
+  function isNameFree(name) {
+    // Check if username is in use
+    var free = true;
+    this._sessions.each(function(s) {
+      if (s.id === name) {
+        free = false;
+      }
+    });
+    return free;
+  }
+}
+
+// Build an instance of the Server object and call start() to get Express going
+var serverInstance = Server();
+serverInstance.start();
